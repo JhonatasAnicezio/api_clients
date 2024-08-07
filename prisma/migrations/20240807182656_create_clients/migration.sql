2dbb52dfda60bebcc17838c5c0c987a4447b8266
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-08-07 15:29:24 -0300

    feat: generate migrate at table clients

---
+++ prisma/migrations/20240807182656_create_clients/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "clients" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" INTEGER NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "clients_email_key" ON "clients"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "clients_phone_key" ON "clients"("phone");
