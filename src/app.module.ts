0d0dcc207269d62573383506519e245a8117377c
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-08-14 07:43:30 -0300

    feat: add configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule } from './clients/clients.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [ClientsModule],
+  imports: [
+    ClientsModule,
+    ConfigModule.forRoot(),
+    AuthenticationModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
