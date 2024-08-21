92a1411ba76c6755cf883253511835d0a394b877
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-08-21 18:39:20 -0300

    test: git

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://grolet-patisserie.vercel.app/*', // Permitir requisições apenas dessa origem
+    origin: 'https://grolet-patisserie.vercel.app', // Permitir requisições apenas dessa origem
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
     credentials: true, // Permite o envio de cookies nas requisições
   });
 
