0acd389e8a0ef89e19ae25c9822a621d926c39fe
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-08-21 13:18:54 -0300

    feat: alter local app front

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', // Permitir requisições apenas dessa origem
+    origin: 'https://grolet-patisserie.vercel.app/', // Permitir requisições apenas dessa origem
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
     credentials: true, // Permite o envio de cookies nas requisições
   });
